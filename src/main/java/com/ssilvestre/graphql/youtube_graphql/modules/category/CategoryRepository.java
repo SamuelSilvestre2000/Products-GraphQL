cb86ef3c566700703d68c8d440a746621fc473e3
Samuel Silvestre <samuelsilvestre2000@gmail.com>
2023-08-30 10:27:49 -0300

    Add files via upload

---
+++ src/main/java/com/ssilvestre/graphql/youtube_graphql/modules/category/CategoryRepository.java
@@ -0,0 +1,8 @@
+package com.ssilvestre.graphql.youtube_graphql.modules.category;
+import java.util.UUID;
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface CategoryRepository extends CrudRepository<CategoryEntity, UUID> {
+    
+}
